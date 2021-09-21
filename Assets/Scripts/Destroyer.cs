37f2b1af2191a6e0a5f6015f4683e3ccca281683
Sekar <fs@sekars-laptop.local>
2021-09-22 01:26:24 +0700

    Project File

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if( tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
